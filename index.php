ce723e9d2de2785096acc0a3cdaf28b5c7733d02
jlo19 <jlo19@DESKTOP-64BNQSP.Home>
2017-04-22 21:21:17 +0100

    Rename from controller to presenter

--- index.php
+++ index.php
@@ -6,32 +6,32 @@ include 'bootstrap.php';
 use app\middleware\Authentication;
 
 $app = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
-
+// adds presenters to slim container
 $container = $app->getContainer();
-$container['ChordController'] = function() {
-    return new \app\controllers\ChordController();
+$container['ChordPresenter'] = function() {
+    return new \app\presenters\ChordPresenter();
 };
-$container['SongController'] = function() {
-    return new \app\controllers\SongController();
+$container['SongPresenter'] = function() {
+    return new \app\presenters\SongPresenter();
 };
-$container['UserController'] = function() {
-    return new \app\controllers\UserController();
+$container['UserPresenter'] = function() {
+    return new \app\presenters\UserPresenter();
 };
 
 // /chords route
-$app->get('/chords', 'ChordController:getChords')->add(new Authentication());
+$app->get('/chords', 'ChordPresenter:getChords')->add(new Authentication());
 // /songs route
-$app->get('/songs', 'SongController:getSongs')->add(new Authentication());
+$app->get('/songs', 'SongPresenter:getSongs')->add(new Authentication());
 // /users route
-$app->post('/users', 'UserController:addUser');
+$app->post('/users', 'UserPresenter:addUser');
 // /users/login route
-$app->post('/users/login', 'UserController:login');
+$app->post('/users/login', 'UserPresenter:login');
 // /users/{id} route
-$app->put('/users/{id}', 'UserController:editUser')->add(new Authentication());
-$app->get('/users/{id}', 'UserController:getUserDetails')->add(new Authentication());
+$app->put('/users/{id}', 'UserPresenter:editUser')->add(new Authentication());
+$app->get('/users/{id}', 'UserPresenter:getUserDetails')->add(new Authentication());
 // /users/{id}/chords route
-$app->get('/users/{id}/chords', 'UserController:getUserChords')->add(new Authentication());
-$app->post('/users/{id}/chords', 'UserController:addUserChord')->add(new Authentication());
-$app->put('/users/{id}/chords', 'UserController:updateUserChords')->add(new Authentication());
+$app->get('/users/{id}/chords', 'UserPresenter:getUserChords')->add(new Authentication());
+$app->post('/users/{id}/chords', 'UserPresenter:addUserChord')->add(new Authentication());
+$app->put('/users/{id}/chords', 'UserPresenter:updateUserChords')->add(new Authentication());
 
 $app->run();
